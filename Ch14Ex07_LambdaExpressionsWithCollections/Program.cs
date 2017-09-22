3f8b2bc6e5f7f364bf10105837863eb326c7fccb
David Prause <praused@hotmail.com>
2017-09-22 11:46:31 -0400

    Add project files.

---
+++ Ch14Ex07_LambdaExpressionsWithCollections/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch14Ex07_LambdaExpressionsWithCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
