b599d57146d1e64ad77d2f109476a3d328a30e7d
David Prause <praused@hotmail.com>
2017-09-22 12:23:35 -0400

    Example complete

--- Ch14Ex07_LambdaExpressionsWithCollections/Program.cs
+++ Ch14Ex07_LambdaExpressionsWithCollections/Program.cs
@@ -10,6 +10,13 @@ namespace Ch14Ex07_LambdaExpressionsWithCollections
     {
         static void Main(string[] args)
         {
+            string[] curries = { "pathia", "jalfrezi", "korma" };
+            Console.WriteLine(curries.Aggregate((a, b) => a + " " + b));
+            Console.WriteLine(curries.Aggregate<string, int>(0, (a, b) => a + b.Length));
+            Console.WriteLine(curries.Aggregate("Some curries:", (a, b) => a + " " + b));
+            Console.WriteLine(curries.Aggregate<string, string, string>("Some curries:", (a, b) => a + " " + b, a => a));
+            Console.WriteLine(curries.Aggregate<string, string, int>("Some curries:", (a, b) => a + " " + b, a => a.Length));
+            Console.ReadKey();
         }
     }
 }
